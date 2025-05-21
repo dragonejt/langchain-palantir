bb772196ca6c99bcce8e0d32cee826881bbec064
Evan Tung <81f3b115-c951-4a0c-981e-1ef480fed5a4>
2025-05-21 01:09:04 +0000

    add package name

--- src/setup.py
+++ src/setup.py
@@ -7,8 +7,8 @@ from setuptools import find_packages, setup
 setup(
     name=os.environ["PKG_NAME"],
     version=os.environ["PKG_VERSION"],
-    description="My Python library project",
-    author="hakase-insights",
+    description="LangChain using Palantir LLMs",
+    author="dragonejt",
     packages=find_packages(exclude=["contrib", "docs", "test"]),
     # Please specify your dependencies in conda_recipe/meta.yaml instead.
     install_requires=[],

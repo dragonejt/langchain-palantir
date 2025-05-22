1cccc9405357fabc49b7c2b706b7e6c1e61bcf92
Evan Tung <81f3b115-c951-4a0c-981e-1ef480fed5a4>
2025-05-22 00:52:42 +0000

    Autogenerated commit message at May 21, 2025, 8:52 PM

--- src/test/test_chat_openai.py
+++ src/test/test_chat_openai.py
@@ -1,12 +1,13 @@
 from unittest import TestCase
 from palantir_models.models import OpenAiGptChatLanguageModel
 
-from langchain_palantir.chat_models.openai_gpt import ChatOpenA
+from langchain_palantir.chat_models.openai_gpt import PalantirChatOpenAI
 
 
 def test_response() -> None:
     question = "Why is the sky blue?"
-    wrapped_llm = ChatOpenAI(model=OpenAiGptChatLanguageModel.get("GPT_4o"))
+    model = OpenAiGptChatLanguageModel.get("GPT_4o")
+    wrapped_llm = PalantirChatOpenAI(model=model)
     answer = wrapped_llm.invoke(question)
 
     assert len(answer.content) >= 0

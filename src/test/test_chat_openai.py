d4309fa501431e7a86b94f04cf1d4142d49060a9
Evan Tung <81f3b115-c951-4a0c-981e-1ef480fed5a4>
2025-05-22 01:04:47 +0000

    remove linting

--- src/test/test_chat_openai.py
+++ src/test/test_chat_openai.py
@@ -4,10 +4,12 @@ from palantir_models.models import OpenAiGptChatLanguageModel
 from langchain_palantir.chat_models.openai_gpt import PalantirChatOpenAI
 
 
-def test_response() -> None:
+def test_response() -> str:
     question = "Why is the sky blue?"
     model = OpenAiGptChatLanguageModel.get("GPT_4o")
     wrapped_llm = PalantirChatOpenAI(model=model)
     answer = wrapped_llm.invoke(question)
 
     assert len(answer.content) >= 0
+
+    return answer.content
